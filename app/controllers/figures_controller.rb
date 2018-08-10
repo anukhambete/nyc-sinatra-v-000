5c3a9a0f3b665a3c2689c4a1e7e4cfd4c7092bf4
Anu Khambete <anukhambete@gmail.com>
2018-08-10 02:10:35 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -42,7 +42,7 @@ class FiguresController < ApplicationController
       @figure.save
     end
     @figure.save
-    redirect to "/figures/#{@landmark.id}"
+    redirect to "/figures/#{@figure.id}"
   end
 
   post '/figures' do
